fabcb8df291c8f25d644967313a6e44931c2ca79
vikash0624 <vy2304446@gmail.com>
2024-11-05 16:59:12 +0530

    Create Account and Login Page added

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import Footer from "./HomeComponents/Footer";
+import Header from "./HomeComponents/Header";
+import LoginPageBody from "./LoginPageComponent/LoginPageBody";
+
+const LoginPage = () => {
+    return (
+      <>
+         <Header></Header>
+         <LoginPageBody></LoginPageBody>
+         <Footer></Footer>
+      </>
+    )
+}
+
+export default LoginPage;
