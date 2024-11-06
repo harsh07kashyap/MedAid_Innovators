a7a30de026099c63d5cb18a6ccdc359ea9632103
vikash0624 <vy2304446@gmail.com>
2024-11-06 09:57:51 +0530

    styling of create account and login page added

--- frontend/src/pages/CreateAccountComponent/CreateAccountBody.jsx
+++ frontend/src/pages/CreateAccountComponent/CreateAccountBody.jsx
@@ -4,24 +4,26 @@ import styles from "./CreateAccountBody.module.css"
 const CreateAccuntBody = () => {
    return (
     <>  <div className={styles.topform}>
-          <form className={styles.formClass}>
+          <form className={`${styles.formClass} flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg`}>
             <div className={`${styles.form} `}>
-                 <p>Create Account</p>
-                 <p>Please sign up to book appointment</p>
+                 <p className={styles.para1}>Create Account</p>
+                 <p className={styles.para2}>Please sign up to book appointment</p>
                  <div >
-                   <p>Full Name</p>
-                   <input type="text" required="" value=""/>
+                   <p className={styles.para3}>Full Name</p>
+                   <input className={styles.input} type="text" required="" value=""/>
                  </div>
                 <div class="w-full ">
-                  <p>Email</p>
-                 <input  type="email" required="" value=""/>
+                  <p className={styles.para3}>Email</p>
+                 <input className={styles.input} type="email" required="" value=""/>
                  </div>
                 <div class="w-full">
-                  <p>Password</p>
-                  <input  type="password" required="" value=""/>
+                  <p className={styles.para3}>Password</p>
+                  <input className={styles.input} type="password" required="" value=""/>
                 </div>
-                <button class="bg-primary text-white w-full py-2 my-2 rounded-md text-base">Create account</        button>
-                <p>Already have an account? <span class="text-primary underline cursor-pointer">
+                <div className={styles.createTop}>
+                <button  className={`${styles.create} bg-primary text-white w-full py-2 my-2 rounded-md text-base`}>Create account</        button>
+                </div>
+                <p className={styles.para2}>Already have an account? <span class="text-primary underline cursor-pointer">
                   <Link to="/loginPage">login</Link>
                   </span></p>
                 </div>
