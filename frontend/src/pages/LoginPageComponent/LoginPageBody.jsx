e46ddaead349367e288d419a865c8b8c60f1a24a
harsh07kashyap <kashyap7.harsh@gmail.com>
2024-11-07 19:41:05 +0530

    User(patient) registration functionality added

--- frontend/src/pages/LoginPageComponent/LoginPageBody.jsx
+++ frontend/src/pages/LoginPageComponent/LoginPageBody.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./LoginPageBody.module.css"
 const localhosts="http://localhost:4000"
-import { React, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LoginPageBody = () => {
